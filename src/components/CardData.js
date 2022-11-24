fc3dca14ee590e7d9eff9df7e7700b7d42737732
ChrisKling <iggster14@gmail.com>
2022-11-24 13:57:29 +0100

    added sellerID to cardData

--- src/components/CardData.js
+++ src/components/CardData.js
@@ -1,5 +1,6 @@
 const CardData = [
   {
+    sellerId: "WritAnyRandomNameWeWant",
     img: "https://images.pexels.com/photos/53588/tomatoes-vegetables-food-frisch-53588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Ton of Tomatoes",
     description: `I got 1 ton of tomatoes to trade, I'm in the dragon's Den area Would
@@ -8,6 +9,7 @@ const CardData = [
     expiry: "4 days",
   },
   {
+    sellerId: "WritAnyRandomNameWeWant",
     img: "https://images.pexels.com/photos/691166/pexels-photo-691166.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Orange Oranges",
     description: `Oranges for days come and get em now!`,
@@ -15,6 +17,7 @@ const CardData = [
     expiry: "9 days",
   },
   {
+    sellerId: "WritAnyRandomNameWeWant",
     img: "https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Appealing Apples",
     description: `I got some delicious apples no strings attached, you need to try them`,
@@ -22,6 +25,7 @@ const CardData = [
     expiry: "10 days",
   },
   {
+    sellerId: "WritAnyRandomNameWeWant",
     img: "https://images.pexels.com/photos/3568039/pexels-photo-3568039.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Combersome Cucumbers",
     description: `Why didn't I just do a cheaky little Lorem15 here? idk I guess I'm going crazy typing all this myself`,
@@ -29,6 +33,7 @@ const CardData = [
     expiry: "2 days",
   },
   {
+    sellerId: "WritAnyRandomNameWeWant",
     img: "https://images.pexels.com/photos/547263/pexels-photo-547263.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     title: "Corny Corn",
     description: `Corn Corn Corn Corn Corn Corn Corn Corn Corn CornCorn Corn Corn Corn Corn Corn Corn Corn I like corn`,
@@ -36,6 +41,7 @@ const CardData = [
     expiry: "8 weeks",
   },
   {
+    sellerId: "WritAnyRandomNameWeWant",
     img: "https://images.pexels.com/photos/7657242/pexels-photo-7657242.jpeg?auto=compress&cs=tinysrgb&w=1600",
     title: "Great Grapes",
     description: `Need some grapes? I got em hit me up as soon as you got this`,
