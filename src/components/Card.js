e5a041ff3a50b6c20a3a76e7ae96fc6e8dcd7f3b
Danni Malka <dannimalka.iag@gmail.com>
2022-11-24 13:51:06 +0200

    added grid to listings

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,29 +2,27 @@ import { Button } from "@mui/material";
 import React from "react";
 import "./Card.css";
 
-function Card() {
+function Card(props) {
   return (
-    <div className="listingCardContainer">
-      <div className="imageContainer">
-        <img
-          src="https://images.pexels.com/photos/53588/tomatoes-vegetables-food-frisch-53588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt=""
-        />
-      </div>
-      <div className="cardContent">
-        <div className="cardTitle">
-          <h3 className="titleh3">Fresh Tomatoes</h3>
+    <div className="grid">
+      {props.details.map((value, index) => (
+        <div className="listingCardContainer" key={index}>
+          <div className="imageContainer">
+            <img src={value.img} alt="" />
+          </div>
+          <div className="cardContent">
+            <div className="cardTitle">
+              <h3 className="titleh3">{value.title}</h3>
+            </div>
+            <div className="cardBody">
+              <p className="bodyp">{value.description}</p>
+            </div>
+          </div>
+          <p>{value.location}</p>
+          <p>{value.expiry}</p>
+          <button className="listingCardButton">Open Listing</button>
         </div>
-        <div className="cardBody">
-          <p className="bodyp">
-            I got 5kg of tomatoes to trade, I'm in the dragon's Den area Would
-            love to trade it for some bananas HMU!
-          </p>
-        </div>
-      </div>
-      <p>Location: my place</p>
-      <p>Listing expiry: 4 days</p>
-      <button className="listingCardButton">Open Listing</button>
+      ))}
     </div>
   );
 }
--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "./Card";
 import "./Listings.css";
+import CardData from "./CardData";
 
 function Listings() {
   return (
@@ -8,7 +9,7 @@ function Listings() {
       <div>
         <h1>Listings</h1>
         <div className="listingCardsWrapper">
-          <Card />
+          <Card details={CardData} />
         </div>
       </div>
     </div>
