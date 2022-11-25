0989ae3723f285d2be2292df4cf9fb12aaf198a2
Danni Malka <dannimalka.iag@gmail.com>
2022-11-25 11:25:36 +0200

    cleaned up unused files and lines of code

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "./Card";
 import "./Listings.css";
 import CardData from "./CardData";
 import { ArrowBack } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
+import Filter from "./filter";
 
 function Listings() {
   function navigateToPage(e, value) {
     navigator(value);
   }
+
+  const [Filtered, setFiltered] = useState([]);
+
   const navigator = useNavigate();
   return (
     <div>
@@ -24,7 +28,9 @@ function Listings() {
         <div>
           <div className="listingsHeader">
             <h1>Listings</h1>
-            <div className="searchContainer"></div>
+            <div className="searchContainer">
+              <Filter />
+            </div>
           </div>
           <div className="listingCardsWrapper">
             <Card details={CardData} />
