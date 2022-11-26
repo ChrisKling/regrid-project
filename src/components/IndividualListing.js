c057794c8e605a9c2b8131921275e68c86e89776
Danni Malka <dannimalka.iag@gmail.com>
2022-11-26 20:58:34 +0200

    layout and stylings added to individual listings page

--- src/components/IndividualListing.js
+++ src/components/IndividualListing.js
@@ -1,16 +1,17 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./IndividualListing.css";
 import CardData from "./CardData";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { ArrowBack } from "@mui/icons-material";
 
 function IndividualListing() {
+  const navigator = useNavigate();
   const [product, setProduct] = useState();
-  //new usestate for loading  = true
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
 
   useEffect(() => {
-    console.log("hi", product);
     if (product) {
       setLoading(false);
     }
@@ -25,11 +26,36 @@ function IndividualListing() {
   }
 
   return (
-    <div className="listingsContainer individual">
-      <h1> welcome {id}</h1>
-      <p>{product.title}</p>
-
-      <p></p>
+    <div className="listingsContainer" id="individualListingContainer">
+      <button
+        className="navigateBackButton"
+        onClick={() => {
+          navigator(-1);
+        }}
+      >
+        <ArrowBack />
+      </button>
+      <div className="listingImgContainer">
+        <img src={product.img} alt="" />
+      </div>
+      <div className="listingProductContainer">
+        <div className="h1Container">
+          <h1>{product.title}</h1>
+        </div>
+        <div className="listingInfoContainer">
+          <h2>Listing Info</h2>
+          <p>Seller ID: {product.sellerId}</p>
+          <p>Product ID: {product.id}</p>
+          <p>Expiry Date: {product.expiry}</p>
+          <p>Listing Date: {product.startDate}</p>
+        </div>
+        <div className="listingDescriptionContainer">
+          <p>{product.description}</p>
+        </div>
+        <div className="listingButtonContainer">
+          <button id="individualListingButton">Send Trade Request</button>
+        </div>
+      </div>
     </div>
   );
 }
