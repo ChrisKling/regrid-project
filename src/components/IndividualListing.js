7e02ff4d74b4e2a946ebceb46adf0c3ef9e6e874
Danni Malka <dannimalka.iag@gmail.com>
2022-11-26 18:20:03 +0200

    pages are now created dynamically when a new id is pressed

--- src/components/IndividualListing.js
+++ src/components/IndividualListing.js
@@ -1,15 +1,33 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./IndividualListing.css";
 import CardData from "./CardData";
 import { useParams } from "react-router-dom";
 
 function IndividualListing() {
-  const [data, setData] = useState({ CardData });
+  const [product, setProduct] = useState();
+  //new usestate for loading  = true
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
+
+  useEffect(() => {
+    console.log("hi", product);
+    if (product) {
+      setLoading(false);
+    }
+  }, [product]);
+
+  useEffect(() => {
+    setProduct(CardData.find((product) => product.id === parseInt(id)));
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading..</div>;
+  }
 
   return (
     <div className="listingsContainer individual">
       <h1> welcome {id}</h1>
+      <p>{product.title}</p>
 
       <p></p>
     </div>
